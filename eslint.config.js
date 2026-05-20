c7f2576c1c021601e401378b707cdad29053c8f7
Ariel Soares <soaresariel017@gmail.com>
2026-05-20 18:31:16 -0300

    feat: add fastify api

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
-
+import globals from "globals";
+import tseslint from "typescript-eslint";
+  
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
