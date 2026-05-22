440477ce0beff8aa2383018edc9e3f6265ece8ab
Ariel Soares <soaresariel017@gmail.com>
2026-05-22 01:26:34 -0300

    chore: "add scalar docs setup and ORM Prisma "

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,135 @@
-import "dotenv/config"
+import "dotenv/config";
 
-import Fastify from 'fastify'
-import { serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod';
+import fastifyCors from "@fastify/cors";
+import fastifySwagger from "@fastify/swagger";
+import fastifyApiReference from "@scalar/fastify-api-reference";
+import { fromNodeHeaders } from "better-auth/node";
+import Fastify from "fastify";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
 import z from "zod";
 
+import { auth } from "./lib/auth.js";
 
 const app = Fastify({
-  logger: true
-})
+  logger: true,
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-
-
-app.withTypeProvider<ZodTypeProvider>().route({
-    method: 'GET',
-    url: "/",
-    schema: {
-        description: "Hello World route", 
-        tags: ["Hello World"],
-        response: {
-            200: z.object({
-                message: z.string(), 
-            }),
-        },
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Bootcamp Treinos API",
+      description: "API para bootcamp",
+      version: "1.0.0",
     },
-    handler: () => {
-        return{
-            message: "Hello World ",
-        }
-    }
-
+    servers: [
+      {
+        description: "Localhost",
+        url: "https://localhost:3000",
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
 });
 
 
+
+await app.register(fastifyCors, {
+  origin: ["https://localhost:8081"],
+  credentials: true,
+})
+
+await app.register(fastifyApiReference, {
+  routePrefix: "/docs",
+  configuration: {
+    sources: [
+      {
+        title: "Bootcamp Treinos API",
+        slug: "bootcamp-treinos-api",
+        url: "/swagger.json",
+      },
+      {
+        title: "Auth API",
+        slug: "auth-api",
+        url: "/api/auth/open-api/generate-schema",
+      },
+    ],
+  },
+});
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/swagger.json",
+  schema: {
+    hide: true,
+  },
+  handler: async () => {
+    return app.swagger();
+  },
+});
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/",
+  schema: {
+    description: "Hello World route",
+    tags: ["Hello World"],
+    response: {
+      200: z.object({
+        message: z.string(),
+      }),
+    },
+  },
+  handler: () => {
+    return {
+      message: "Hello World ",
+    };
+  },
+});
+
+
+app.route({
+  method: ["GET", "POST"],
+  url: "/api/auth/*",
+  async handler(request, reply) {
+    try {
+      // Construct request URL
+      const url = new URL(request.url, `http://${request.headers.host}`);
+      
+      // Convert Fastify headers to standard Headers object
+      const headers = fromNodeHeaders(request.headers);
+      // Create Fetch API-compatible request
+      const req = new Request(url.toString(), {
+        method: request.method,
+        headers,
+        ...(request.body ? { body: JSON.stringify(request.body) } : {}),
+      });
+      // Process authentication request
+      const response = await auth.handler(req);
+      // Forward response to client
+      reply.status(response.status);
+      response.headers.forEach((value, key) => reply.header(key, value));
+      return reply.send(response.body ? await response.text() : null);
+    } catch (error) {
+      app.log.error(error)
+      return reply.status(500).send({ 
+        error: "Internal authentication error",
+        code: "AUTH_FAILURE"
+      });
+    }
+  }
+});
+
 try {
-  await app.listen({ port: Number(process.env.PORT) })
+  await app.listen({ port: Number(process.env.PORT) });
 } catch (err) {
-  app.log.error(err)
-  process.exit(1)
+  app.log.error(err);
+  process.exit(1);
 }
